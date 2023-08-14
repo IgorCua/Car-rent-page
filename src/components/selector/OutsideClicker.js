75511f0eb47577bce43e67b60d0409676e296f5c
IhorC <igorchua@gmail.com>
2023-08-14 17:20:09 -0500

    list custom components added

---
+++ src/components/selector/OutsideClicker.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from "react";
+
+export function OutsideClicker(props) {
+  const wrapperRef = useRef(null);
+  let { isActive, setActive } = props;
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setActive((isActive = false));
+        }
+      }
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [ref]);
+  }
+
+  useOutsideAlerter(wrapperRef);
+
+  return <div ref={wrapperRef}>{props.children}</div>;
+}
